66d22bf2450c8b7ea9e89f84a117d4b4140a6bef
fubar2 <ross.lazarus@gmail.com>
2024-04-02 12:58:02 +1100

    remove some cruft from ssl hackery for https uri fasta.fai grabbing

--- jbrowse2/jbrowse2.py
+++ jbrowse2/jbrowse2.py
@@ -481,22 +481,6 @@ class JbrowseConnector(object):
         }
         return wstyle
 
-    def urllib_get_2018():
-        # Using a protected member like this is not any more fragile
-        # than extending the class and using it. I would use it.
-        url = "https://localhost:6667/my-endpoint"
-        ssl._create_default_https_context = ssl._create_unverified_context
-        with urllib.request.urlopen(url=url) as f:
-            print(f.read().decode("utf-8"))
-
-    def urllib_get_2022():
-        # Finally! Able to use the publice API. Happy happy!
-        url = "https://localhost:6667/my-endpoint"
-        scontext = ssl.SSLContext(ssl.PROTOCOL_TLS)
-        scontext.verify_mode = ssl.VerifyMode.CERT_NONE
-        with urllib.request.urlopen(url=url, context=scontext) as f:
-            print(f.read().decode("utf-8"))
-
     def process_genomes(self, genomes):
         assembly = []
         assmeta = []
